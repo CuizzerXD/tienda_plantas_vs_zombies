c3b5b7379983de70f3035e7977f1c5b1256c3ac5
CuizzerXD <c280978d@gmail.com>
2026-03-11 20:08:48 -0600

    pequeños cambios

--- js/main.js
+++ js/main.js
@@ -43,6 +43,34 @@ function validaFormulario() {
   localStorage.setItem("comentarios", JSON.stringify(mensajes));
 }
 
-function filtradoTienda(){
-  
+function filtradoTienda(orden) {
+  //Traemos de dentro del elemento main el elemento que concuerde con la clase .row ya que este es el contenedor
+  //que tiene dentro todas las cards
+  const row = document.querySelector('main .row.g-4.mb-5');
+  //Si no existe se sale de la funcion
+  if (!row) return;
+
+  // Solo selecciona columnas que son hijos directos de la clase rows
+  // :scope significa el elemento actual en este caso el row, el mayor que (>) sirve para apuntar al hijo directo
+  //Basicamente aqui lo que se hace es meter en un NodeList todos los elementos que concuerden con la clase
+  //cool md-4 y col-sm-6 para despues agregarlos a un arreglo para poder utilizar el metodo sort
+  const columnas = Array.from(row.querySelectorAll(':scope > .col-md-4.col-sm-6'));
+
+  //El metodo sort sirve para ordenar colA y colB son los div.col-md-4.col-sm-6
+  columnas.sort((colA, colB) => {
+    const cardA = colA.querySelector('.planta-card');
+    const cardB = colB.querySelector('.planta-card');
+    //El signo ? vendria siendo una condicional llamada optional chaining y ?? se llama nullish coalescing
+    const precioA = parseInt(cardA?.dataset.precio ?? 0, 10);
+    const precioB = parseInt(cardB?.dataset.precio ?? 0, 10);
+
+    // 'mayor-menor'
+    if (orden === 'mayor-menor') {
+      return precioB - precioA;  
+    }
+    // 'menor-mayor'
+    return precioA - precioB;    
+  });
+
+  columnas.forEach((col) => row.appendChild(col));
 }
